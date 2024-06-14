92b607e296c80eadad279717aa6a99972c516f51
Miguel S. Achury <miguelachury@hotmail.com>
2024-06-13 20:26:01 -0500

    creando servicios de reserva de recursos relacionando tablas

--- src/main/java/co/edu/unbosque/servicios/ReservaRecursoServicios.java
+++ src/main/java/co/edu/unbosque/servicios/ReservaRecursoServicios.java
@@ -1,4 +1,25 @@
 package co.edu.unbosque.servicios;
 
-public class ReservaRecursoServicios {
+import co.edu.unbosque.modelo.dao.ReservaRecursoDAO;
+import co.edu.unbosque.modelo.dto.ReservaRecursoDTO;
+import co.edu.unbosque.modelo.entidades.ReservaRecurso;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import org.modelmapper.ModelMapper;
+
+import java.io.Serializable;
+
+@Stateless
+public class ReservaRecursoServicios implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public ModelMapper modelMapper = new ModelMapper();
+
+    @Inject
+    ReservaRecursoDAO reservaRecursoDAO;
+
+    public ReservaRecursoDTO crearReservaRecurso(ReservaRecursoDTO reservaRecurso) {
+        System.out.println("en el servicio de las reservas de los recursos, creando: " + reservaRecurso.toString());
+        return modelMapper.map(reservaRecursoDAO.crear(modelMapper.map(reservaRecurso, ReservaRecurso.class)), ReservaRecursoDTO.class);
+    }
 }
